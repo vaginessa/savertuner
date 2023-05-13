ec0e6d85d224f3dc9623aa4fd75cb3e00f0f48ca
sim <git@sgougeon.fr>
2023-05-13 17:04:39 +0200

    Upgrade dependencies

--- app/src/main/java/com/draco/buoy/fragments/MainPreferenceFragment.kt
+++ app/src/main/java/com/draco/buoy/fragments/MainPreferenceFragment.kt
@@ -44,12 +44,12 @@ class MainPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnS
 
     override fun onResume() {
         super.onResume()
-        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        preferenceManager.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
     }
 
     override fun onPause() {
         super.onPause()
-        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
